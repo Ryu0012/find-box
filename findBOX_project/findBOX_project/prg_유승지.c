a5e1e07b1c59348d145f03b3a079adb5a9c568ea
LAPTOP-OG78C8F4\82109 <ryu9426seung@gmail.com>
2021-11-06 20:33:25 +0900

    혼자 betting 구현 다른 플레이어랑 betting 미구현

--- findBOX_project/findBOX_project/prg_유승지.c
+++ findBOX_project/findBOX_project/prg_유승지.c
@@ -16,13 +16,28 @@
 void printing();
 void setting(BOX, BOX, BOX);
 void start_p(BOX, BOX, BOX);
-bool bet_result();	// 미구현
 int betting();		// 미구현
 
-// 배팅 결과 (성공, 실패)
-bool bet_result() {
+// @ 들어있는 상자 위치
+int key = L;
 
-}
+// @이가 들어있는 상자번호
+int p_key = 1;
+
+// 상자 배열
+char arr[N] = { 0 };
+
+// 초기 사용자가 가지고 있는 코인
+int playerCoin = 500;
+
+// 섞는 속도, 횟수
+int t = 10, cnt = L;
+
+// 상자
+typedef struct {
+	int pol;	// 현위치
+	int det;	// 진행방향
+}BOX;
 
 // 배팅_금액
 int betting() {
@@ -32,27 +47,12 @@ int betting() {
 
 }
 
-// 상자
-typedef struct {
-	int pol;	// 현위치
-	int det;	// 진행방향
-}BOX;
-
-// @ 들어있는 상자 위치
-int key = L;
-
-// player의 key 값 ( 상자 번호 )
-int p_key = 1;
-
-// 상자 배열
-char arr[N] = { 0 };
-
-// 초기 사용자가 가지고 있는 코인
-int coin = 500;
-
 // 상자 배열 출력
 void printing() {
 	printf("\n");
+	for (int i = 0; i < 4; i++) {
+		printf(" ");
+	}
 	for (int i = 0; i < N; i++) {
 		printf("%c", arr[i]);
 	}
@@ -62,8 +62,6 @@ void printing() {
 // # 교환, 왼쪽에 있는 # : box_1, 오른쪽에 있는 # : box_2
 // 교환과 상관없는 #은 유지 : maintain
 void setting(BOX box_1, BOX box_2, BOX maintain) {
-
-	int t = 10;
 
 	// key 가 교환하려는 #상자에 있는 경우 -> 위치 변경
 	// 아니면 그대로 유지
@@ -101,7 +99,6 @@ void setting(BOX box_1, BOX box_2, BOX maintain) {
 
 		printing();
 	}
-	t--; // 턴이 증가할때마다 속도 점점 빠르게
 }
 
 // 초기 화면
@@ -116,19 +113,18 @@ void start_p(BOX box1, BOX box2, BOX box3) {
 	arr[box2.pol] = '#';
 	arr[box3.pol] = '#';
 
-
 	printing();
 }
 
 // 랜덤하게 상자섞기
 void randomMix(BOX box1, BOX box2, BOX box3) {
 
-	int randNum = 0, cnt = L;	// randNum: 상자 섞을 수 있는 경우를 랜덤하게 뽑음, cnt: 섞는 횟수
+	int randNum = 0;	// randNum: 상자 섞을 수 있는 경우를 랜덤하게 뽑음, cnt: 섞는 횟수
 	srand((unsigned int)time(NULL));
 
 	for (int i = 0; i < cnt; i++) {
 		// Sleep(3000); // 확인용
-		randNum = rand() % 4 + 1;
+		randNum = rand() % 3 + 1;
 
 		// 상자를 섞을 수 있는 경우( 1과 2 , 2와 3, 1과 3의 상자가 섞이는 경우 )
 		switch (randNum) {
@@ -139,48 +135,79 @@ void randomMix(BOX box1, BOX box2, BOX box3) {
 		case 3:
 			setting(box1, box3, box2); continue;
 		}
-		cnt++;	// 1 턴이 지나갈때 마다 상자를 섞는 횟수 증가
 	}
+	cnt++;	// 1 턴이 지나갈때 마다 상자를 섞는 횟수 증가
+	t--;	// 1 턴이 지나고 섞는 속도 증가
 }
 
+
+
 int main(void) {
+	// 혼자게임 / 인공지능과 함께게임 / 나가기
+
 	// @ 위치
-	printf("@위치는 [%d]상자에 있습니다.\n", p_key);
-	Sleep(1000);
-	system("cls");
+	// 턴 ( 총 5턴 )
 
 	BOX box1 = { L };		// L = 3
 	BOX box2 = { 2 * L + 1 };	// 7
 	BOX box3 = { 3 * L + 2 };	// 11
 
-	// 초기 화면
-	start_p(box1, box2, box3);
+	for (int turn = 1; turn <= N; turn++) {
+		printf("%d 턴 입니다.\n", turn);
+		printf("@위치는 [%d]상자에 있습니다.\n", p_key);
+		Sleep(1000);
+		system("cls");
 
-	// 상자 섞기
-	randomMix(box1, box2, box3);
+		// 초기 화면
+		start_p(box1, box2, box3);
 
-	// @가 어느 상자에 들어있는지 확인용
-	int k = 0;
-	printf("k = ");
-	scanf_s("%d", &k);
+		// 상자 섞기
+		randomMix(box1, box2, box3);
+		printf("\n");
 
-	// 상자 배열을 1번상자, 2번상자, 3번상자로 바꿈
-	switch (key) {
-	case 3:
-		p_key = 1; break;
-	case 7:
-		p_key = 2; break;
-	case 11:
-		p_key = 3; break;
-	}
+		// @가 어느 상자에 들어있는지 확인용
+		int playerKey = 0, bettingCoin = 0;	// player가 고른 상자
+		printf("몇 번째 상자를 고르시겠습니까?\n");
+		printf("[ 1번상자 = 1, 2번상자 = 2, 3번상자 = 3 ]\n");
+		printf("=> ");
+		scanf_s("%d", &playerKey);
+		printf("\n");
 
-	// key 의 위치 확인용
-	if (k == p_key) {
-		printf("맞음\n");	// 코인 얻음
-	}
-	else
-		printf("@이 없음\n");	// 코인 잃음
+		printf("몇 코인을 betting 하시겠습니까? (양수값으로 입력해주세요)\n");
+		printf("=> ");
+		scanf_s("%d", &bettingCoin);
+		printf("\n");
+
+		// 상자 배열을 1번상자, 2번상자, 3번상자로 바꿈
+		switch (key) {
+		case 3:
+			p_key = 1; break;
+		case 7:
+			p_key = 2; break;
+		case 11:
+			p_key = 3; break;
+		}
 
 
+		// player 배팅 결과
+		if (playerKey == p_key) {
+			playerCoin = playerCoin + (bettingCoin * turn);
+			printf("betting에 성공하셨습니다.\n");
+			printf("%d * %d = %d 코인을 얻었습니다.\n", bettingCoin, turn, bettingCoin * turn);			
+		}
+		else {
+			playerCoin -= bettingCoin;
+			printf("betting에 실패하셨습니다.\n");
+			printf("%d 코인을 잃었습니다.\n", bettingCoin);
+		}
+
+		printf("\n");
+		printf("현재 가지고 있는 코인은 %d 입니다.\n", playerCoin);
+		printf("기다리면 다음 턴으로 넘어갑니다.\n");
+
+		Sleep(3000);
+		system("cls");
+	} 
+	
 	return 0;
 }
